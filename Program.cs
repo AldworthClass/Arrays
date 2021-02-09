56021b93b663aeeaaf3a714324088b9268cde26f
Steve Aldworth <s.aldworth@tvdsb.ca>
2021-02-09 14:30:05 -0500

    Solutions to challenges added.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-
+            Random generator = new Random();
             string[] names = {"Trinity", "Neo", "Morpheus", "Agent Smith", "Tank"};  
             Console.WriteLine(names[1] + " is 'The One'.");
             Console.WriteLine(names[3] + " is the bad guy.");
@@ -18,10 +18,18 @@ namespace Arrays
             Console.WriteLine("There are " + names.Length + " names in my array.");
             names[3] = "Agent Smith";
             Console.WriteLine(names[3] + " is the bad guy.");
+            
+            // Change the last name without hard coding an index number
             names[names.Length - 1] = "Cypher";
             Console.WriteLine(names[names.Length - 1] + " is another bad guy.");
 
+            // Random Challenge
             Console.WriteLine();
+            Console.WriteLine("Here is a random name:");
+            Console.WriteLine(names[generator.Next()]);
+            Console.WriteLine();
+
+            // Prints all elements no matter what the size of the array is
             for (int i = 0; i < names.Length; i++)
             {
                 Console.WriteLine(i + ". " + names[i]);
